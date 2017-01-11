194aa0b687cee3804a840c546b3ce8d11810f623
Morgan Handley <morganhandley@Morgans-MacBook-Pro.local>
2017-01-11 11:48:23 -0700

    Initial commit

---
+++ exercises/Objects/objects.js
@@ -0,0 +1,22 @@
+//var person = {
+//    fullName: 'Tommy Boy',
+//    age: 26,
+//    weight: 306,
+//    friends: ['Richard', 'Michelle']
+//};
+//
+//var whatProperty = prompt("what property do you want");
+//
+//console.log(person[whatProperty]);
+
+//var person = {name: 'Frodo', age: 33};  
+//var new_person = person;
+//
+//new_person.name = 'Sam';
+//
+//console.log(person.name);  
+//console.log(new_person.name); 
+
+
+
+
---
+++ exercises/arrays/arrays.js
@@ -0,0 +1,33 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];  
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];  
+
+//console.log("fruit: ", fruit);  
+//console.log("vegetables: ", vegetables);  
+
+vegetables.pop();
+//fruit.splice(0);
+fruit.indexOf("orange");
+//fruit.push(indexOf"orange");
+//vegetable.length();
+
+vegetable.splice(vegetables.length-1);
+fruit.splice(0,1); //0 is index, splice 1, shift method also
+
+var orangeIndex = fruit.indexOf("orange");
+fruit.push(fruit.indexOf"orange");
+
+
+console.log(vegetables.length);
+vegetable.push(vegetable.length); //boils down to three
+
+var food = fruit.concat(vegetables);
+console.log(food);
+
+food.splice(4,2);
+
+food.reverse();
+
+console.log(food.toString());
+
+
+
